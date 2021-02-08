620c777f8dbb5b89d0a63bbe57704b64142508dc
Pablo Segesser <pablosegesser@gmail.com>
2021-02-08 15:10:46 -0300

    env vars

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: process.env.API_ENDPOINT
         })
     }
     
